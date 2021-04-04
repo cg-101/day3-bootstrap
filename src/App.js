bfc76bce1fd0a008af0b48e4cd1160cede03619b
Santosh Mondal <santosh.ece06@gmail.com>
2021-04-04 12:39:10 +0530

    use case 3

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="bg-primary d-flex justify-content-center " style={{}}>
+    <div className="bg-primary d-flex justify-content-between" style={{}}>
       <div className="bg-info">Child 1</div>
       <div className="bg-success">Child 2 </div>
-      <div className="bg-warning">Child 3 </div>
     </div>
   );
 }
