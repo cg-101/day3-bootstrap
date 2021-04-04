3a772420231c499800da6674c60e74a9a73db222
Santosh Mondal <santosh.ece06@gmail.com>
2021-04-04 12:41:07 +0530

    managing spalces.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="bg-primary d-flex justify-content-between" style={{}}>
-      <div className="bg-info">Child 1</div>
-      <div className="bg-success">Child 2 </div>
+      <div className="bg-info col">Child 1</div>
+      <div className="bg-success col">Child 2 </div>
+      <div className="bg-warning col">Child 3 </div>
     </div>
   );
 }
