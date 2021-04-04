50e7dc00d4e78bec39ea87bd790d679cecb8c633
Santosh Mondal <santosh.ece06@gmail.com>
2021-04-04 12:55:39 +0530

    layout 1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,43 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="bg-primary d-flex justify-content-between" style={{}}>
-      <div className="bg-info col">Child 1</div>
-      <div className="bg-success col">Child 2 </div>
-      <div className="bg-warning col">Child 3 </div>
+    <div className="container-fluid">
+      <div
+        className="row bg-primary text-light d-flex align-items-center pl-2"
+        style={{ height: 55 }}
+      >
+        My Project
+      </div>
+
+      <div className="row d-flex ">
+        <div
+          className="bg-info col-3 m-1 d-flex justify-content-center align-items-center"
+          style={{ height: 100 }}
+        >
+          A
+        </div>
+
+        <div
+          className="bg-success col-3 m-1 d-flex justify-content-center align-items-center"
+          style={{ height: 100 }}
+        >
+          A
+        </div>
+
+        <div
+          className="bg-info col-3 m-1 d-flex justify-content-center align-items-center"
+          style={{ height: 100 }}
+        >
+          A
+        </div>
+
+        <div
+          className="bg-info col-3 m-1 d-flex justify-content-center align-items-center"
+          style={{ height: 100 }}
+        >
+          A
+        </div>
+      </div>
     </div>
   );
 }
